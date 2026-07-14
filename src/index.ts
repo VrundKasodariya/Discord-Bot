385b6ca888348aa0574c406cf8303dbd31f5f8f5
Vrund Kasodariya <vrundkasodariya@gmail.com>
2026-07-14 16:02:59 +0530

    Add Client

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import 'dotenv/config';
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 
-// const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
 
-// client.on(Events.ClientReady, readyClient => {
-//   console.log(`Logged in as ${readyClient.user.tag}!`);
-// });
+client.on("messageCreate",(message)=>{
+  if(message.author.bot) return;
+  message.reply({
+    content : "Hi From bot"
+  });
+})
 
-// client.on(Events.InteractionCreate, async interaction => {
-//   if (!interaction.isChatInputCommand()) return;
+client.login(process.env.TOKEN);
 
-//   if (interaction.commandName === 'ping') {
-//     await interaction.reply('Pong!');
-//   }
-// });
-// client.login(process.env.TOKEN);
-
-console.log(process.env.TOKEN);
